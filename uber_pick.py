34803efc00aa546f9c8cf4cf59042db6a72c8333
jvmorgan <jhoselyn.villagomez@gmail.com>
2024-01-28 18:39:54 -0600

    se cambia barra deslizante por boton multiple

--- uber_pick.py
+++ uber_pick.py
@@ -29,10 +29,16 @@ hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)#graficar
 
 #Vamos hacer un filtro para el mapa para mostrar solo el recojo a las 17 
-hour_to_filter = st.slider('hour', 0, 23, 17 ) 
+horas = data[DATE_COLUMN].dt.hour.unique()
+hour_to_filter = st.selectbox('hour', horas, 17) 
 filter_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Mapa of all pickups at {hour_to_filter}:00')
 st.map(filter_data)
 
 
+#Vamos hacer un filtro para el mapa para mostrar solo el recojo a las 17 
+#hour_to_filter = st.slider('hour', 0, 23, 17 ) 
+#filter_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+#st.subheader(f'Mapa of all pickups at {hour_to_filter}:00')
+#st.map(filter_data)
 
