33d9d89b5f53e477a753dac80af3e07223cbcd60
jvmorgan <jhoselyn.villagomez@gmail.com>
2024-01-28 18:20:44 -0600

    Se agrego mapa y boton de opcion para mostrar la data cruda

--- uber_pick.py
+++ uber_pick.py
@@ -19,9 +19,20 @@ data_load_state = st.text('Loading data...')
 data = load_data(10000)
 
 data_load_state = st.text('Loading data...done!!!')
-st.subheader('Raw data example')
-st.write(data.head(10))
+if st.checkbox('Show row data'): # para convertir en boton
+    st.subheader('Raw data example')
+    st.write(data.head(10))
+
 #histograma para ver cuáles son las horas de mayor actividad de Uber en la ciudad de Nueva York. Para comenzar, agreguemos un subtítulo justo debajo de la sección de datos sin procesar:
 st.subheader('Number of pickups by hours')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+st.bar_chart(hist_values)#graficar
+
+#Vamos hacer un filtro para el mapa para mostrar solo el recojo a las 17 
+hour_to_filter = st.slider('hour', 0, 23, 17 ) 
+filter_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Mapa of all pickups at {hour_to_filter}:00')
+st.map(filter_data)
+
+
+
